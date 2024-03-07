5179ccde98327d07f2a29e137342221617a67d55
Antonio-Strignano <a.strignano@yahoo.it>
2024-03-07 17:20:14 +0100

    ADD: sviluppato l'esercizio e dà una password

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-let name = prompt('Inserisci il tuo nome') //string
-console.log(name)
+let nome = prompt('Inserisci il tuo nome') //string | null
+console.log(nome)
+
+let cognome = prompt('Inserisci il tuo cognome') //string | null
+console.log(cognome)
+
+let colore = prompt('Qual è il tuo colore preferito?') //string | null
+console.log(colore)
+
+let numero = prompt('Indica un numero da utilizzare')
+console.log(numero)
+
+let pswd = nome + cognome + colore + numero
+console.log(pswd)
